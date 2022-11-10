5c9c6c6ba091798d0f2f5067ead28cb3edd08173
luis.lins <luis.lins@linx.com.br>
2022-11-10 11:55:21 -0300

    commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <header></header>
+      <aside></aside>
+      <main></main>
+      <footer></footer>
+      <div className="header2"></div>
+    </div>
+  );
+}
+
+export default App;
